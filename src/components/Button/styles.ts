6107ef461d00bd9632652a87fba139acaac06858
joao Hitallo <joaohitallo97@gmail.com>
2021-11-20 23:22:16 -0300

    style:  Estilizaçao inicial  da signUp,Button e Input

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,30 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 0 1.5rem;
+  height: 4rem;
+  background: var(--green);
+  color: #FFF;
+  border-radius: 0.25rem;
+  border: 0;
+  font-size: 1rem;
+  margin-top: 1.5rem;
+  font-weight: 600;
 
+  button {
+    padding: 18px;
+    border: 0;
+    border-radius: 18px;
+    background-color: #00c8b3;
+    color: #ffffff;
+
+
+    transition: filter 0.2s;
+        
+    &:hover {
+      filter: brightness(0.7);
+    }
+  }
 `;
--- src/pages/signUp/styles.ts
+++ src/pages/signUp/styles.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  
 
+
+  form {
+    padding: 18px;
+  }
 `;
